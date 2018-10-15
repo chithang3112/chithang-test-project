1265883f488785753686bd9f44e0666d208e97c8
chithang3112 <chithang@pro-seeds.co.jp>
2018-10-15 10:14:03 +0900

    fix webhook

--- webhook.js
+++ webhook.js
@@ -6,6 +6,21 @@ let express = require('express');
 var app = express();
 var httpServer = http.createServer(app);
 //webhook作成
-app.get('/',function(req, res){
-    res.redirect('/login');
+app.post('/webhook',function(req, res){
+    var headers = {
+        'Content-Type': 'application/json',
+    }
+    var body = {
+    }
+    var url = 'https://api.line.me/v2/bot/message/reply';
+    request({
+        url: url,
+        method: 'POST',
+        headers: headers,
+        body: body,
+        json: true
+    });
+    res.status(200).end();
 });
+
+httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
