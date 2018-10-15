54ca4f7fb41e759e37ca2c50a4abea2f0983197f
chithang3112 <chithang@pro-seeds.co.jp>
2018-10-15 10:53:54 +0900

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 let http = require('http');
 
 var request = require('request');
-var apiKey = 'kZzCDStci3DETl1yaWoEWY9YWTnptueBwzv8mOdnccoAdLoR3pj47datZ2R51K5p'
+var apiKey = 'kZzCDStci3DETl1yaWoEWY9YWTnptueBwzv8mOdnccoAdLoR3pj47datZ2R51K5p';
 var defaultUrl = 'https://esk-sys.backlog.jp/api/v2/';
 // var action = 'issues/LW3_SHUKAN-2152'
 var action = 'users';
--- webhook.js
+++ webhook.js
@@ -9,13 +9,23 @@ let PORT = process.env.PORT || 80;
 var app = express();
 var httpServer = http.createServer(app);
 //webhook作成
+
 app.post('/webhook',function(req, res){
     var headers = {
         'Content-Type': 'application/json',
     }
     var body = {
     }
-    var url = 'https://shou-web-hook.herokuapp.com/index';
+
+	var apiKey = 'kZzCDStci3DETl1yaWoEWY9YWTnptueBwzv8mOdnccoAdLoR3pj47datZ2R51K5p';
+	var defaultUrl = 'https://esk-sys.backlog.jp/api/v2/';
+	// var action = 'issues/LW3_SHUKAN-2152'
+	var action = 'users';
+	// var params = '&projectId[]=73975';
+	var params = '';
+	var url = defaultUrl + action +'?apiKey='+apiKey+params;
+
+    // var url = 'https://shou-web-hook.herokuapp.com/index';
     request({
         url: url,
         method: 'POST',
@@ -23,12 +33,6 @@ app.post('/webhook',function(req, res){
         body: body,
         json: true
     });
-    console.log(res);
-    res.status(200).end();
-});
-app.get('/index',function(req, res){
-	console.log(req);
-	console.log(res);
     res.status(200).end();
 });
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
