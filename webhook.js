cec92cef89c3fec1c4e844985c320c3b060d750f
chithang3112 <chithang@pro-seeds.co.jp>
2018-10-15 10:58:38 +0900

    test

--- webhook.js
+++ webhook.js
@@ -11,29 +11,20 @@ var httpServer = http.createServer(app);
 //webhook作成
 
 app.post('/webhook',function(req, res){
-    var headers = {
-        'Content-Type': 'application/json',
-    }
-    var body = {
-    }
-
 	var apiKey = 'kZzCDStci3DETl1yaWoEWY9YWTnptueBwzv8mOdnccoAdLoR3pj47datZ2R51K5p';
 	var defaultUrl = 'https://esk-sys.backlog.jp/api/v2/';
 	// var action = 'issues/LW3_SHUKAN-2152'
 	var action = 'users';
 	// var params = '&projectId[]=73975';
 	var params = '';
-	var url = defaultUrl + action +'?apiKey='+apiKey+params;
-
-    // var url = 'https://shou-web-hook.herokuapp.com/index';
-    request({
-        url: url,
-        method: 'POST',
-        headers: headers,
-        body: body,
-        json: true
-    });
-    res.status(200).end();
-    console.log(res);
+	var uri = defaultUrl + action +'?apiKey='+apiKey+params;
+	var options = {
+	  uri: uri,
+	  headers: {
+	  },
+	};
+	request.get(options, function(error, response, body){
+		console.log(body);
+	});
 });
 httpServer.listen(PORT, () => console.log('Running!!! Listenning on ' + PORT));
